fdd71a0fcd4f1b4be094b775f1e1afccb3af1b5f
Gabriel Vasconcelos <138534514+devitruvius@users.noreply.github.com>
2023-12-25 20:27:51 -0300

    Portfolio upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+// Function to update the <span> element based on the data-name of the active item in the Carousel
+
+
+function updateCertificateText() {
+    const activeCarouselItem = document.querySelector('.carousel-item.active');
+
+    const dataName = activeCarouselItem.querySelector('img').getAttribute('data-name');
+    
+    const certificateSpan = document.querySelector('.certificate');
+    
+    certificateSpan.textContent = dataName;
+}
+
+updateCertificateText();
+
+const certificateCarousel = document.getElementById('certificateCarousel');
+
+certificateCarousel.addEventListener('slid.bs.carousel', updateCertificateText);
